216a552f05cf30cdddf848a17175f458eea89403
Bhupender <bhupenderrao1999@gmail.com>
2025-07-14 13:00:27 +0530

    temp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "payment=*, fullscreen=(self), geolocation=*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
