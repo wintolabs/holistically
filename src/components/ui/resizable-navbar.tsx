60dab70862874875467051988d53f5b01e00cfa6
Bhupender <bhupenderrao1999@gmail.com>
2025-07-15 15:28:51 +0530

    fix: header nav items bg

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -122,7 +122,7 @@ export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
           {hovered === idx && (
             <motion.div
               layoutId="hovered"
-              className="absolute inset-0 h-full w-full rounded-full bg-gray-100 dark:bg-neutral-800"
+              className="absolute inset-0 h-full w-full rounded-full bg-gray-200 dark:bg-neutral-800"
             />
           )}
           <span className="relative z-20">{item.name}</span>
